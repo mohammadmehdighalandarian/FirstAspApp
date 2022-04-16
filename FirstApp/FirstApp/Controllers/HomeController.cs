72414ba86486f1db4e53fcbaaa50ea2c54eea191
mohammadmehdighn@gmail.com <mohammadmehdighn@gmail.com>
2022-04-17 01:43:45 +0430

    create contact us and index

--- FirstApp/FirstApp/Controllers/HomeController.cs
+++ FirstApp/FirstApp/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace FirstApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult ContactUs()
         {
             return View();
         }
